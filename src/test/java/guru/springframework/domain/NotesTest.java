cf140a83c0d126e27964b7cf2bc1a9f5fb1e617e
yul <yu.li@tecsys.com>
2019-03-07 17:20:02 -0500

    Add JUnit5 dependencies

---
+++ src/test/java/guru/springframework/domain/NotesTest.java
@@ -0,0 +1,21 @@
+//: guru.springframework.domain.NotesTest.java
+
+
+package guru.springframework.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NotesTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGetId() {
+	}
+
+}///:~
